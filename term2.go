261160c06d6c7a99547b6afab7316c3adbdd0559
flyerge <kakhak@gmail.com>
2023-03-15 12:47:12 +0400

    Update term2.go

--- term2.go
+++ term2.go
@@ -9,7 +9,7 @@ import (
 )
 
 type Term2 struct {
-	w1  *net.Conn
+	con *net.Conn
 	out chan string
 }
 
@@ -25,19 +25,19 @@ func New(port string) *Term2 {
 	host.WriteString(":")
 	host.WriteString(port)
 
-	t2conn, err := net.Dial("tcp", host.String())
+	t2con, err := net.Dial("tcp", host.String())
 	if err != nil {
 		fmt.Println(err)
 		usage()
 	}
 	//defer t2conn.Close()
 	t2 := &Term2{
-		w1:  &t2conn,
+		con: &t2con,
 		out: make(chan string),
 	}
 	go func() {
 		for elem := range t2.out {
-			fmt.Fprint(*t2.w1, elem)
+			fmt.Fprint(*t2.con, elem)
 		}
 	}()
 	return t2
@@ -54,8 +54,8 @@ func (t2 *Term2) Prnl(str string) *Term2 {
 	return t2
 }
 
-//Clear screen, cursor on top
 func (t2 *Term2) Cls() *Term2 {
-	t2.Prn("\033[H\033[2J")
+	clscmd := "\033[G\033[2K"
+	t2.Prn(clscmd)
 	return t2
 }
